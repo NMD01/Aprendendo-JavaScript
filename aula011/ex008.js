64c313ac545f094e83e3cbf7d3981c60d9b88e76
Nathan Martini Duarte <77181059+NMD01@users.noreply.github.com>
2021-03-13 02:08:47 +0300

    Add files via upload

---
+++ aula011/ex008.js
@@ -0,0 +1,7 @@
+var vel = 88
+console.log(`A velocidade do seu carro é de ${vel} km/h`)
+if(vel>60){ //condição simples
+    console.log('Voce ultrapassou o liite de velocidade de 60km/h. MULTADO!')
+}
+
+console.log('Dirija sempre com o cinto de segurança')
---
+++ aula011/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'Brasil'
+console.log(`Eu nasci na ${pais}`)
+if(pais != 'Brasil'){ //condição composta
+    console.log('vocé não é brasileiro!')
+}else{
+    console.log('Voce é brasileiro')
+}
